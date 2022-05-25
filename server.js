cfae59dc1ece6863d1b5d79bb74b438b0bc5c4bc
Piyush Pawar <piyushpawar199954@gmail.com>
2022-05-25 10:58:01 +0530

    Added product model with Belongs to association with  category

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const configs = require("./config/serverconfigs");
 const categoryRoutes = require("./routes/category.routes");
 const app = express();
 
+const Product = require("./models/index").Product;
+const Categories = require("./models/index").Categories;
 //We need a middleware that will help express to ready all query  and body params, below code is for config.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 categoryRoutes(app);
 
-app.listen(configs.PORT, () => {
-  console.log("server started");
+app.get("/home", async function (req, res) {
+  const getproducts = await Product.findAll({ include: Categories });
+  res.json(getproducts);
+});
+
+app.listen(configs.PORT, async () => {
+  /* const newProduct = await Product.create({
+    name: "Ipad",
+    cost: 100000,
+    description: "apple ipad",
+    categoryId: 1,
+  }); */
 });
