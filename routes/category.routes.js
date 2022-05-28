30aa39a1e331a9b8d0913fec35a3a46e6396bb8e
Piyush Pawar <piyushpawar199954@gmail.com>
2022-05-29 00:16:10 +0530

    Added category creation validator middleware

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,10 +1,15 @@
 const CategoryController = require("../controllers/category.controller");
+const Validator = require("../middleware/requestValidator");
 
 const routes = (app) => {
   //the app parameter is express app only
   //route -> controller fun
   app.get("/ecomm/api/v1/categories", CategoryController.getAllCategories);
-  app.post("/ecomm/api/v1/categories", CategoryController.createCategory);
+  app.post(
+    "/ecomm/api/v1/categories",
+    Validator.validateCategoryCreationRequest,
+    CategoryController.createCategory
+  );
   app.delete("/ecomm/api/v1/categories/:id", CategoryController.deleteCategory);
   app.get("/ecomm/api/v1/categories/:id", CategoryController.getCategory);
   app.get(
